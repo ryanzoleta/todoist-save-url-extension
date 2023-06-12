49f0b00c6fa8334d93a49bab51fdbef8d550bd33
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-06-12 10:20:52 +0800

    feat: save current url to inbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
+import { useState } from 'react';
+import { TodoistApi } from '@doist/todoist-api-typescript';
+
 function App() {
+  const [isSaving, setIsSaving] = useState(false);
+
+  const addUrlToTodoist = (url: string) => {
+    const api = new TodoistApi('YOUR_TOKEN_HERE');
+
+    api
+      .addTask({
+        content: `Check out ${url}`,
+      })
+      .then(() => {
+        setIsSaving(false);
+      });
+  };
+
+  const handleClick = () => {
+    setIsSaving(true);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+
+      if (activeTab.url) {
+        addUrlToTodoist(activeTab.url);
+      }
+    });
+  };
+
   return (
     <main>
       <div className="w-72 p-5">
-        <button className="rounded-full bg-red-500 w-full p-3 hover:bg-red-700 text-red-50">
-          Save URL to Todoist
-        </button>
+        {isSaving ? (
+          <button className="rounded-full bg-red-300 w-full p-3  text-red-50" disabled>
+            Saving...
+          </button>
+        ) : (
+          <button
+            className="rounded-full bg-red-500 w-full p-3 hover:bg-red-700 text-red-50"
+            onClick={handleClick}
+          >
+            Save URL to Todoist
+          </button>
+        )}
       </div>
     </main>
   );
