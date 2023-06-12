d2f3e14cc552e4ed1f4bdcd2a6da54ec3bbbdad8
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-06-12 09:11:47 +0800

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
